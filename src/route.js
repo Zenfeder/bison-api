6be09578f01ab4f81cf54885d0c203df2031b29f
CG <1054609489@qq.com>
2018-03-16 22:16:02 +0800

    test docker

--- src/route.js
+++ src/route.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
         })
         .get((req, res, next) => {
             res.json({
-                message: 'Hello docker'
+                message: 'Hello docker!'
             })
         })
 
