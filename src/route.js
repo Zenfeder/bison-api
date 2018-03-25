35b35c589b6a5db1d8ab6ebae6c1f00602d66dfd
CG <1054609489@qq.com>
2018-03-25 22:59:01 +0800

    test pm2

--- src/route.js
+++ src/route.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
         })
         .get((req, res, next) => {
             res.json({
-                message: '>>> Hello, LinXiaobao'
+                message: '>>> Hello, LinXiaobao...'
             })
         })
 
