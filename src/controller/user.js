573fefce363c500ccd97cedecf44c681be0e834e
CG <1054609489@qq.com>
2018-04-13 17:15:19 +0800

    【优化】用户注册检测用户名重复

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,12 +1,9 @@
 const Auth = require('@controller/auth.js')
 const Email = require('@controller/email.js')
 const UserModel = require('@model/user.js')
-const { 
-    getRandomCode, 
-    validateEmail, 
-    validateUsername,
-    validatePassword
-} = require('@utils/helper.js')
+const JokeModel = require('@model/joke.js')
+const { getRandomCode, validateEmail, validateUsername, validatePassword } = require('@utils/helper.js')
+const { USER_JOKES_TYPES } = require('@utils/constDef.js')
 
 const bcrypt = require('bcryptjs')
 
@@ -66,6 +63,8 @@ class User extends Auth {
             UserModel.findOne({ email }, (err, user) => {
                 if (err) 
                     return reject({ code: 500, message: '验证码认证失败' })
+                if (user && user.name) 
+                    return reject({ code: 403, message: '邮箱已被注册' })
                 if (!user) 
                     return reject({ code: 403, message: '请先获取验证码' })
                 if (user.vcode !== (vcode - 0))
@@ -100,16 +99,23 @@ class User extends Auth {
                 if (!user.verified)
                     return reject({ code: 403, message: '请先校验验证码' })
 
-                user.name = name
-                user.email = email
-                user.password = bcrypt.hashSync(password, 8)
-                user.created_at = Date.now()
-
-                user.save((err, doc) => {
+                UserModel.findOne({ name }, (err, doc) => {
                     if (err) 
                         return reject({ code: 500, message: '用户注册失败' })
+                    if (doc) 
+                        return reject({ code: 403, message: '用户名已被占用' })
 
-                    resolve(this.jwtSign({ user_id: doc._id }))
+                    user.name = name
+                    user.email = email
+                    user.password = bcrypt.hashSync(password, 8)
+                    user.created_at = Date.now()
+
+                    user.save((err, doc) => {
+                        if (err) 
+                            return reject({ code: 500, message: '用户注册失败' })
+
+                        resolve(this.jwtSign({ user_id: doc._id }))
+                    })
                 })
             })
         })
@@ -144,11 +150,46 @@ class User extends Auth {
         return new Promise((resolve, reject) => {
             UserModel.findById(user_id, (err, doc) => {
                 if (err) 
-                    return reject({ code: 500, message: '用户查找失败' })
+                    return reject({ code: 500, message: '数据查找失败' })
                 if (!doc) 
                     return reject({ code: 404, message: '用户不存在' })
 
                 resolve({ id: doc._id, name: doc.name, email: doc.email })
+            })
+        })
+    }
+
+    async jokes({ token, type, offset, size }) {
+        let { user_id } = await this.jwtVerify(token)
+
+        return new Promise((resolve, reject) => {
+            UserModel.findById(user_id, (err, user) => {
+                if (err)
+                    return reject({ code: 500, message: '数据查找失败' })
+                if (!user)
+                    return reject({ code: 404, message: '用户不存在' })
+
+                JokeModel.find({
+                    '_id': {
+                        $in: user[USER_JOKES_TYPES[type]]
+                    }
+                }, (err, jokes) => {
+                    if (err)
+                        return reject({ code: 500, message: '数据查找失败' })
+
+                    let jokeList = []
+                    jokes.forEach(elem => {
+                        jokeList.push({
+                            id: elem._id,
+                            content: elem.content,
+                            like_num: elem.like_user_ids.length,
+                            dislike_num: elem.dislike_user_ids.length,
+                            comment_num: elem.comment_ids.length
+                        })
+                    })
+
+                    resolve(jokeList)
+                })
             })
         })
     }
