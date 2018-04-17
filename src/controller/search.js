be5c0becce98cbe7a9d5fd00b9bc438ae6786b74
CG <1054609489@qq.com>
2018-04-17 15:15:02 +0800

    【新增】搜索关键字分词、分页获取搜索内容接口完成

---
+++ src/controller/search.js
@@ -0,0 +1,55 @@
+const nodejieba = require('nodejieba')
+const JokeModel = require('@model/joke.js')
+
+class Search {
+    constructor() {}
+
+    hotKeyword () {
+
+    }
+
+    result ({ keyword, offset, size }) {
+        let keywordList = nodejieba.cut(keyword)
+        let keywordStr = ''
+        keywordList.forEach((elem, index) => {
+            keywordStr += elem
+
+            if (index < keywordList.length - 1)
+                keywordStr += '|'
+
+            if (index === keywordList.length - 1)
+                keywordStr += '+' 
+        })
+
+        return new Promise((resolve, reject) => {
+            JokeModel.find({
+                content: new RegExp(keywordStr, 'i')
+            })
+            .skip(offset)
+            .limit(size)
+            .exec((err, docs) => {
+                if (err) 
+                    return reject({ code: 500, message: '数据查找失败' })
+
+                let jokes = []
+
+                docs.forEach(elem => {
+                    jokes.push({
+                        id: elem._id,
+                        content: elem.content,
+                        user_id: elem.user_id,
+                        user_name: elem.user_name,
+                        user_avator: elem.user_avator,
+                        like_num: elem.like_user_ids.length,
+                        dislike_num: elem.dislike_user_ids.length,
+                        comment_num: elem.comment_ids.length,
+                        created_at: elem.created_at
+                    })
+                })
+                resolve(jokes)
+            })
+        })
+    }
+}
+
+module.exports = Search
--- src/route/modules/search.js
+++ src/route/modules/search.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Search = require('@controller/search.js')
+
+let search = null
 
 router.use((req, res, next) => {
-  next()
+    search = new Search()
+
+    next()
 })
 
 // 获取热搜关键字
@@ -12,7 +17,15 @@ router.get('/hot', (req, res) => {
 
 // 获取搜索结果列表（分页）
 router.get('/', (req, res) => {
-    
+    let keyword = req.query.keyword === undefined?'':req.query.keyword
+    let offset = req.query.offset===undefined?0:(Number.isInteger(req.query.offset-0)?req.query.offset-0:0)
+    let size = req.query.size===undefined?10:(Number.isInteger(req.query.size-0)&&req.query.size-0>0?req.query.size-0:10)
+
+    search.result({ keyword, offset, size }).then(data => {
+        res.status(200).send({ data })
+    }).catch(err => {
+        res.status(err.code).send({ message: err.message })
+    })
 })
 
 module.exports = router
