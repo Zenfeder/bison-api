7b5b0e0939773b08f1c0b72e5de2e690151717d1
CG <1054609489@qq.com>
2018-04-24 18:21:09 +0800

    【修改】接口添加api前缀

--- src/route/entry.js
+++ src/route/entry.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
         next()
     })
 
-    app.use('/user', require('@route/modules/user.js'))
-    app.use('/joke', require('@route/modules/joke.js'))
-    app.use('/search', require('@route/modules/search.js'))
+    app.use('/api/user', require('@route/modules/user.js'))
+    app.use('/api/joke', require('@route/modules/joke.js'))
+    app.use('/api/search', require('@route/modules/search.js'))
 }
